69cf3ac447ddc19380567796b4dc343d382d77d8
gonzalojpv <gonzalojpv@gmail.com>
2020-03-29 22:47:23 -0600

    GP, svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,42 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Bahlum</title>
+</svelte:head>
+
+<h1>Hi, I’m Gonzalo P. Vargas</h1>
+<h3 class="text-center">Full-Stack Developer</h3>
+
+<p><strong>Open your ... mind, soul and source.</strong></p>
