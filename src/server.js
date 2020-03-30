d69e5857863f81a2d1ae137c1e96a35a8a9b6670
gonzalojpv <gonzalojpv@gmail.com>
2020-03-29 23:56:28 -0600

    GP, test

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-    'gonzalojpv.github.io',
+    'bahlum',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
