3cee5fc5adb02c19d5f26871e66d7c7869ec5fbd
gonzalojpv <gonzalojpv@gmail.com>
2020-03-31 23:08:52 -0600

    GP, about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -11,9 +11,21 @@
   <Row>
     <Col cols="12">
       <h1>About me</h1>
-      <p>I'm Gonzalo, Full-Stack Developer from Acala, Chiapas, México. I like write code, mis principales fortalezas son la programación en PHP, Javascript, y Python. Soy GNU/Linux de corazón, mi distro favorito es OpenSUSE con Xfce.</p>
-      <p>Estoy en constante aprendizaje, ya que cada año la tecnología va evolucionando rapidamente, me encanta solucionar problemas y en especial los nuevos retos.</p>
-      <p>Strong engineering professional graduated from ITTG.</p>
+      <p>I'm Gonzalo, Full-Stack Web Developer living at <a href="https://www.google.com/maps/place/Acala,+Chis./@16.5577435,-92.8171038,15z/data=!3m1!4b1!4m5!3m4!1s0x85ed34bd03bc608d:0x401fafd4ec65730!8m2!3d16.5543648!4d-92.8053731" target="_blank">Chiapas</a> .
+      I like write code, main skill is PHP, Databases, Javascript, Web Development, and Python(jr). I'm GNU/Linux (OpenSUSE with Xfce) user.</p>
+
+      <h3>Full-Stack Web Developer</h3>
+      <p>Since 2010, Gonzalo has been making a living as web developer, however, his interest in applications WEB + LAMP began long before.</p>
+      <p>Currently I work building WebApps, creating tailored WordPress websites, applications WEB + LAMP by using frameworks(Codeigniter and Laravel)
+      and mixtures of PHP,Javascript, JQuery, Vuejs, Svelte, Css, Sass, Git and such.</p>
+
+      <p>Strong engineering professional graduated from <a href="https://www.tuxtla.tecnm.mx/" target="_blank">ITTG</a>.</p>
+
+      <hr>
+
+      <blockquote class="blockquote">
+        <p class="mb-0 text-center">I'm fat but at least I'm not a windows</p>
+      </blockquote>
     </Col>
   </Row>
 </Container>
