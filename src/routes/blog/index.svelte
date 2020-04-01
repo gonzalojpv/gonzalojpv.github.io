2f1a3156e66bad763207e73396640f2fc9b896dc
gonzalojpv <gonzalojpv@gmail.com>
2020-04-01 14:37:44 -0600

    GP, test

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -44,7 +44,7 @@
   	<Col cols="12">
   		<hr>
   		<blockquote class="blockquote">
-        <p class="mb-0 text-center">Imagination is more important than knowledge</p>
+        <p class="mb-0 text-center">Imagination is more important than knowledge.</p>
       </blockquote>
   	</Col>
   </Row>
